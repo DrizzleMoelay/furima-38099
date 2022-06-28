4eaf9fa2c028a73277c92dce4b44570419df1d3e
DrizzleMoelay <moemoehtay2025@gmail.com>
2022-06-28 17:50:43 +0900

    Active Hashの導入&バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,5 +2,18 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one    :purchase_record
   has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status_info
+  belongs_to :shipping_charge
+  belongs_to :shipping_area
+  belongs_to :days_to_ship
   
+  validates :name, :description, :price, presence: true
+
+  validatable :category_id,        numericality: { other_than: 1, message: "can't be blank"}
+  validatable :status_info_id,     numericality: { other_than: 1 ,message: "can't be blank"}
+  validatable :shipping_charge_id, numericality: { other_than: 1 ,message: "can't be blank"}
+  validatable :shipping_area_id,   numericality: { other_than: 0 ,message: "can't be blank"}
+  validatable :days_to_ship_id,    numericality: { other_than: 1 ,message: "can't be blank"}
 end
