565f704de33a1347c29eaca83b900b4addf54b31
DrizzleMoelay <moemoehtay2025@gmail.com>
2022-06-29 18:03:33 +0900

    異常系テスト追加

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -13,73 +13,109 @@ RSpec.describe Item, type: :model do
     end
   
     context '出品できない場合' do
-      it 'imageが空では登録できない' do
+      it 'imageが空では出品できない' do
         @item.image = nil
         @item.valid?
         expect(@item.errors.full_messages).to include "Image can't be blank"
       end
 
-      it 'nameが空では登録できない' do
+      it 'nameが空では出品できない' do
         @item.name = ''
         @item.valid?
         expect(@item.errors.full_messages).to include "Name can't be blank"
       end
 
-      it 'descriptionが空では登録できない' do
+      it 'descriptionが空では出品できない' do
         @item.description = ''
         @item.valid?
         expect(@item.errors.full_messages).to include "Description can't be blank"
       end
 
-      it 'category_idが空では登録できない' do
+      it 'category_idが空では出品できない' do
         @item.category_id = ''
         @item.valid?
         expect(@item.errors.full_messages).to include "Category can't be blank"
       end
 
-      it 'status_info_idが空では登録できない' do
+      it 'category_idに「---」が選択されている場合は出品できない' do
+        @item.category_id = '---'
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Category can't be blank"
+      end
+
+      it 'status_info_idが空では出品できない' do
         @item.status_info_id = ''
         @item.valid?
         expect(@item.errors.full_messages).to include "Status info can't be blank"
       end
 
-      it 'shipping_charge_idが空では登録できない' do
+      it 'status_info_idに「---」が選択されている場合は出品できない' do
+        @item.status_info_id = '---'
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Status info can't be blank"
+      end
+
+      it 'shipping_charge_idが空では出品できない' do
         @item.shipping_charge_id = ''
         @item.valid?
         expect(@item.errors.full_messages).to include "Shipping charge can't be blank"
       end
 
-      it 'shipping_area_idが空では登録できない' do
+      it 'shipping_charge_idに「---」が選択されている場合は出品できない' do
+        @item.shipping_charge_id = '---'
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Shipping charge can't be blank"
+      end
+
+      it 'shipping_area_idが空では出品できない' do
         @item.shipping_area_id = ''
         @item.valid?
         expect(@item.errors.full_messages).to include "Shipping area can't be blank"
-        
       end
 
-      it 'days_to_ship_idが空では登録できない' do
+      it 'shipping_area_idに「---」が選択されている場合は出品できない' do
+        @item.shipping_area_id = '---'
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Shipping area can't be blank"
+      end
+
+      it 'days_to_ship_idが空では出品できない' do
         @item.days_to_ship_id = ''
         @item.valid?
         expect(@item.errors.full_messages).to include "Days to ship can't be blank"
         
       end
 
-      it 'priceが空では登録できない' do
+      it 'days_to_ship_idに「---」が選択されている場合は出品できない' do
+        @item.days_to_ship_id = '---'
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Days to ship can't be blank"
+        
+      end
+
+      it 'priceが空では出品できない' do
         @item.price = ''
         @item.valid?
         expect(@item.errors.full_messages).to include "Price is not a number"
         
       end
 
-      it 'priceが299以下では登録できない' do
+      it 'priceが299以下では出品できない' do
         @item.price = '288'
         @item.valid?
         expect(@item.errors.full_messages).to include "Price must be greater than or equal to 300"
       end
 
-      it 'priceが10000000以上では登録できない' do
+      it 'priceが10000000以上では出品できない' do
         @item.price = '10000000'
         @item.valid?
         expect(@item.errors.full_messages).to include "Price must be less than or equal to 9999999"
+      end
+
+      it 'priceに半角数字以外が含まれている場合は出品できない' do
+        @item.price = '100３０'
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Price is not a number"
       end
 
       it 'userが紐付いていないと保存できない' do
