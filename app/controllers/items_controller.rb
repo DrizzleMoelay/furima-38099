02430338a167a8efcdf48d937100c39ca4e53852
DrizzleMoelay <moemoehtay2025@gmail.com>
2022-07-01 18:49:12 +0900

    destroyアクションの編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: [:show, :edit, :update]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
   def index
     @items = Item.all.order(created_at: :desc)
   end
@@ -36,8 +36,9 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    item = Item.find(params[:id])
-    item.destroy
+    if @item.user
+      @item.destroy
+    end
     redirect_to root_path
   end
 
