1dd9a469bc40bcb322daa9eb4fbb105387f3ad14
DrizzleMoelay <moemoehtay2025@gmail.com>
2022-07-01 21:41:10 +0900

    destroyアクションの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -36,10 +36,10 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    if @item.user
+    if @item.user_id == current_user.id
       @item.destroy
+      redirect_to root_path
     end
-    redirect_to root_path
   end
 
   private
