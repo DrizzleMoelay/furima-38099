933c067f10bee245643a19b4cccf66df1e0fe0a8
DrizzleMoelay <moemoehtay2025@gmail.com>
2022-07-01 15:11:16 +0900

    コントローラーにアクションを定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -23,6 +23,15 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    item = Item.find(params[:id])
+    item.update(item_params)
+  end
+
   private
   def item_params
     params.require(:item).permit(:image, :name, :description, :category_id, :status_info_id, :shipping_charge_id, :shipping_area_id, :days_to_ship_id, :price).merge(user_id: current_user.id)
