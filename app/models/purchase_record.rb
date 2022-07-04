7ab1271c343ce314e3ada7692f7b6961ee4d5fed
DrizzleMoelay <moemoehtay2025@gmail.com>
2022-07-04 17:54:16 +0900

    アソシエーションの修正

--- app/models/purchase_record.rb
+++ app/models/purchase_record.rb
@@ -1,5 +1,5 @@
 class PurchaseRecord < ApplicationRecord
-  has_one    : shipping_address
-  belongs_to : user
-  belongs_to : item
+  has_one    :shipping_address
+  belongs_to :user
+  belongs_to :item
 end
