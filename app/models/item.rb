a49b0706229b72afaf8e7b227c5cda9c364d94e5
DrizzleMoelay <moemoehtay2025@gmail.com>
2022-07-10 18:47:53 +0900

    バリデーションを日本語に指定

--- app/models/item.rb
+++ app/models/item.rb
@@ -11,9 +11,9 @@ class Item < ApplicationRecord
   
   validates :image, :name, :description, presence: true
   validates :price, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}, presence: true
-  validates :category_id,        numericality: { other_than: 1, message: "can't be blank"}
-  validates :status_info_id,     numericality: { other_than: 1 ,message: "can't be blank"}
-  validates :shipping_charge_id, numericality: { other_than: 1 ,message: "can't be blank"}
-  validates :shipping_area_id,   numericality: { other_than: 0 ,message: "can't be blank"}
-  validates :days_to_ship_id,    numericality: { other_than: 1 ,message: "can't be blank"}
+  validates :category_id,        numericality: { other_than: 1, message: "を選択してください"}
+  validates :status_info_id,     numericality: { other_than: 1 ,message: "を選択してください"}
+  validates :shipping_charge_id, numericality: { other_than: 1 ,message: "を選択してください"}
+  validates :shipping_area_id,   numericality: { other_than: 0 ,message: "を選択してください"}
+  validates :days_to_ship_id,    numericality: { other_than: 1 ,message: "を選択してください"}
 end
